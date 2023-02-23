4bb9c29dc91943ce4f3e2608373c2d078deb07ac
sree0715 <125061287+sree0715@users.noreply.github.com>
2023-02-23 22:18:21 +0530

    Add files via upload

---
+++ 7_day.py
@@ -0,0 +1,53 @@
+# print(dir({1,3}
+
+# k={1,"hi",'sea',""}
+# (k.clear())
+# print(k)
+
+
+# p={3,"hello",9,6.0,'3.2'}
+# q={"hello",6.7,4,"k"}
+# print(p.difference(q))
+
+# p={3,"hello",9,6.0,'3.2'}
+# p.discard(6.0)
+# print(p)
+
+# p={3,"hello",9,6.0,'3.2'}
+# p.pop()
+# print(p)
+
+# p={3,"hello",9,6.0,'3.2'}
+# q={"hello",6.7,4,"k"}
+# print(p.intersection(q))
+
+
+# p={1,2,3,4,5}
+# q={5,6,7,8,9}
+# print(p.union(q))
+
+
+# p={1,2.3,'hi'}
+# q={2.3,5,'hello'}
+# p.remove(2.3)
+# print(p)
+
+
+# p={1,2,3,4,5}
+# q={6,7,8,9}
+# p.issuperset(q)
+
+# print('P and Q are disjoint?' , p.isdisjoint(q))
+
+# p={1,2,3,4,5}
+# q={1,2,3}
+# print(q.issubset(p))
+# print(p.issuperset(q))
+
+
+
+
+
+
+
+
